e96ea188c999af7a874803b17f0492bb11b10e65
mao <1296193245@qq.com>
2023-06-11 21:06:28 +0800

    data_visualization_highcharts_scatter_and_bubble_charts

---
+++ src/main/java/mao/data_visualization_highcharts_scatter_and_bubble_charts/DataVisualizationHighchartsScatterAndBubbleChartsApplication.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_highcharts_scatter_and_bubble_charts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataVisualizationHighchartsScatterAndBubbleChartsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DataVisualizationHighchartsScatterAndBubbleChartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/data_visualization_highcharts_scatter_and_bubble_charts/DataVisualizationHighchartsScatterAndBubbleChartsApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.data_visualization_highcharts_scatter_and_bubble_charts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataVisualizationHighchartsScatterAndBubbleChartsApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
